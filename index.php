faf943a2788cff631a47ab56c566707d7b895273
temporizer <temporizer@gmail.com>
2014-05-04 21:11:19 -0700

    Updated initial template

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Clark Regional Emergency Services Agency</title>
-</head>
-<body>
-	content here
-</body>
-</html>
+<?php include 'includes/header.inc.php'; ?>
+
+			
+
+<?php include 'includes/footer.inc.php'; ?>
